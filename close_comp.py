0ec73cc729f4a339b6c4c8848a7806db102b3f08
Dmitriy <71712794+viva-cuba@users.noreply.github.com>
2021-07-17 23:46:40 +0300

    Update close_comp.py

--- close_comp.py
+++ close_comp.py
@@ -11,21 +11,36 @@ from time import strftime
 
 
 
+                       
 def listen_command():
     r = sr.Recognizer()
     with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=1)
+        r.pause_threshold = 1
         print("слушаю тебя:")
         audio = r.listen(source)
     try:
-        our_speech = r.recognize_google(audio, language="ru-Ru").lower()
-        print("вы сказали: "+ our_speech)
+        our_speech = r.recognize_google(audio, language="ru-RU").lower()
+        print("вы сказали: "+our_speech)
         return our_speech
     except sr.UnknownValueError:
         return "ошибка"
     except sr.RequestError as e:
+        say_message("проверь интернет")
         return "ошибка"
-    
 
+def do_tris_command(message):
+    message = message.lower()
+
+def say_message(message):
+    
+    voice = gTTS(message, lang="ru")
+    file_voice_name = "_audio_" + \
+    str(random.randint(0, 100))+".mp3"
+    voice.save(file_voice_name)
+    playsound.playsound(file_voice_name)
+    os.remove(file_voice_name)
+    print("Голосовой ассистент: "+message)
 
 class Alarm:    
     def __init__(self, h, m, replay):
@@ -61,63 +76,38 @@ class Alarm:
                     time.sleep(1)
                     say_message('1')
                     say_message('один на верёвочке, один на ниточке, один на паутиночке, паутиночка разрывается и компьютер выключается')
-                    os.system("C:\\Windows\\System32\\Shutdown.exe -s -t 00")
+                    os.system("C:\\Windows\\System32\\Shutdown.exe -s -message 00")
                     exit()
-                
-
-                
-def say_message(message):
-    
-    voice = gTTS(message, lang="ru")
-    file_voice_name = "_audio_" + \
-    str(time.time())+"_"+str(random.randint(0, 100000))+".mp3"
-    voice.save(file_voice_name)
-    playsound.playsound(file_voice_name)
-    os.remove(file_voice_name)
-    print("Голосовой ассистент: "+message)
-
-
-date = strftime("%H:%M")    
-say_message('сейчас '+ date)            
-now = datetime.datetime.now()
-say_message('задай время выключения')
-
-tims = listen_command()
-if now.hour >= 0 and now.hour < 10:
-    tims = '0' + tims
-
-
-
-if tims == "сейчас":
-    say_message('выключаю')
-    os.system("C:\\Windows\\System32\\Shutdown.exe -s -t 00")
+          
    
+if __name__ == '__main__':
+    date = strftime("%H:%M")    
+    say_message('сейчас '+date)
+    say_message('задай время выключения')
+    now = datetime.datetime.now()
+    spik = listen_command()
+    if now.hour >= 0 and now.hour < 10:
+        spik = '0' + spik
+    if spik == 'сейчас':
+        os.system("C:\\Windows\\System32\\Shutdown.exe -s -message 00")
+        exit()    
+    say_message('компьютер отключится в ' + spik )
 
-    
 replay = 1
-
-say_message('компьютер отключится в ' + tims )
 set_hour = True
 h = ""
 m = ""
-for char in tims:
+for char in spik:
     if set_hour:
-    
         if char != ":":
             h += char
         else:
             set_hour = False
     else:
         m += char
-        
+
 a = Alarm(h,m,replay)
 a.signal()
-
-
-    
-
-
-command = listen_command()  # слушает команду
 
 
 
